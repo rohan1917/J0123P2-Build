2c7f60dce647a95e71476afc12a1d63345ef1a4e
Rohan Kinnal <64979784+rohan1917@users.noreply.github.com>
2023-02-25 15:47:09 +0000

    J0123P2-D3Cards

--- index.js
+++ index.js
@@ -44,4 +44,62 @@ const taskModal = document.querySelector(".task__modal__body");
 // console.log(taskContents);
 // console.log(taskModal);
 
-const htmlTaskContent = ({ id, title, description, type, url }) => {};
+// Template for the card on screen
+// elem identifier key=${id} is been misssing on line 50th
+const htmlTaskContent = ({ id, title, description, type, url }) => `
+  <div class="col-md-6 col-lg-4 mt-3" id=${id}>
+    <div class='card shadow-sm task__card'>
+    
+      <div class='card-header d-flex justify-content-end task__card__header'>
+          <button type='button' class='btn btn-outline-primary mr-1.5' name=${id}>
+              <i class='fas fa-pencil-alt name=${id}'></i>
+          </button>
+           <button type='button' class='btn btn-outline-danger mr-1.5' name=${id}>
+              <i class='fas fa-trash-alt name=${id}'></i>
+          </button>
+      </div>
+      <div class='card-body'>
+          ${
+            url &&
+            `<img width='100%' src=${url} alt='Card Image' class='card-img-top md-3 rounded-lg' />`
+          }
+          <h4 class='card-title task__card__title'>${title}</h4>
+          <p class='description trim-3-lines text-muted'>${description}</p>
+          <div class='tags text-white d-flex flex-wrap'>
+            <span> class='badge bg-primary m-1'${type}</span>
+          </div>
+      </div>
+      <div class='card-footer'>
+          <button type='button' class='btn btn-outline-primary float-right' data-bs-toggle="modal" data-bs-target="#showTask">Open Task</button>
+      </div>
+    </div>
+  </div>
+`;
+
+// Modal Body on >> Clk of Open Task
+const htmlModalContent = ({ id, title, description, url }) => {
+  const date = new Date(parseInt(id));
+  return `
+  <div id=${id}>
+     ${
+       url &&
+       //  `<img width='100%' src=${url} alt='Card Image' class='img-fluid place__holder__image mb-3' />`
+       `<img width='100%' src=${url} alt='Card Image' class='img-fluid place__holder__image mb-3' />`
+     }
+     <strong class='text-muted text-sm'>Created on: ${date.toDateString()}</strong>
+     <h2 class='my-3'>${title}</h2>
+     <p class='text-muted'>${description}</p>
+  </div>
+  `;
+};
+
+const updateLocalStorage = () => {
+  localStorage.setItem(
+    "task",
+    JSON.stringify({
+      tasks: state.taskList,
+    })
+  );
+};
+
+// Load Initial Data
