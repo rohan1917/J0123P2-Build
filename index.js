fa47bab116b70ffb6d18ae9670d04e7180065669
Rohan Kinnal <64979784+rohan1917@users.noreply.github.com>
2023-02-28 06:50:34 +0000

    Got cards on UI

--- index.js
+++ index.js
@@ -33,13 +33,14 @@
 //   ],
 // };
 
+// backup storage
 const state = {
   taskList: [],
 };
 
 // DOM Operations
-const taskContents = document.querySelector(".task__contents");
 const taskModal = document.querySelector(".task__modal__body");
+const taskContents = document.querySelector(".task__contents");
 
 // console.log(taskContents);
 // console.log(taskModal);
@@ -47,7 +48,7 @@ const taskModal = document.querySelector(".task__modal__body");
 // Template for the card on screen
 // elem identifier key=${id} is been misssing on line 50th
 const htmlTaskContent = ({ id, title, description, type, url }) => `
-  <div class="col-md-6 col-lg-4 mt-3" id=${id}>
+  <div class="col-md-6 col-lg-4 mt-3" id=${id} key=${id}>
     <div class='card shadow-sm task__card'>
     
       <div class='card-header d-flex justify-content-end task__card__header'>
@@ -93,6 +94,7 @@ const htmlModalContent = ({ id, title, description, url }) => {
   `;
 };
 
+// where we convert json > str (i.e., for local storage)
 const updateLocalStorage = () => {
   localStorage.setItem(
     "task",
@@ -102,4 +104,100 @@ const updateLocalStorage = () => {
   );
 };
 
-// Load Initial Data
+// where we convert str > json (i.e., for rendering the cards on the screen)
+// const loadInitialData = () => {
+//   const localStorageCopy = JSON.parse(localStorage.task);
+
+//   if (localStorageCopy) state.taskList = localStorageCopy.tasks;
+
+//   state.taskList.map((cardDate) => {
+//     // taskContents.innerAdjacentHTML("beforeend", htmlTaskContent(cardDate));
+//     taskContents.insertAdjacentHTML("beforeend", htmlTaskContent(cardDate));
+//   });
+// };
+
+const loadInitialData = () => {
+  const localStorageCopy = JSON.parse(localStorage.task);
+
+  if (localStorageCopy) state.taskList = localStorageCopy.tasks;
+
+  state.taskList.map((cardDate) => {
+    taskContents.insertAdjacentHTML("beforeend", htmlTaskContent(cardDate));
+  });
+};
+
+// Spread Operator
+/**
+ const obj = {
+    name: "rohan",
+    age: 2
+}
+
+
+console.log(obj);
+ {name: 'rohan', age: 2}
+
+console.log({obj});
+ {obj: {…}}obj: {name: 'rohan', age: 2}[[Prototype]]: Object
+
+console.log({...obj});
+ {name: 'rohan', age: 2}
+
+//  appending or adding a new key into obj:
+console.log({...obj, designation: "mentor"});
+{name: 'rohan', age: 2, designation: 'mentor'}
+ */
+
+/**
+ * 
+//  updating key value using spread operator
+const obj={
+    name: "rohan"
+}
+
+console.log(obj)
+ {name: 'rohan'}
+
+
+console.log({...obj, age : 2});
+ {name: 'rohan', age: 2}
+
+console.log({...obj, age :4});
+{name: 'rohan', age: 4}
+ */
+
+/* 
+var date = new Date();
+console.log(Date.now());
+
+1677511569666
+*/
+
+// when we update or when we edit ..we need to save
+const handleSubmit = (event) => {
+  // console.log("event triggerd");
+  const id = `${Date.now()}`;
+  const input = {
+    url: document.getElementById("imageUrl").value,
+    title: document.getElementById("taskTitle").value,
+    type: document.getElementById("tags").value,
+    description: document.getElementById("taskDescription").value,
+  };
+  // if (input.title === "" || input.tags === "" || input.taskDescription === "") {
+  //   return alert("Please fill all the necessary fiels :-)");
+  // }
+
+  // taskContents.innerAdjacentHTML(
+  taskContents.insertAdjacentHTML(
+    "beforeend",
+    htmlTaskContent({ ...input, id })
+  );
+  state.taskList.push({ ...input, id });
+
+  updateLocalStorage();
+};
+
+//open task
+// edit task
+// save edit
+// search
